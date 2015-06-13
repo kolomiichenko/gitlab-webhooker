d2193ff35fa50849b33a3d4892f21fe109721c37
Andrii Kolomiichenko <bboywilld@gmail.com>
2015-06-13 15:05:45 +0700

    init

---
+++ README.md
@@ -0,0 +1,20 @@
+# gitlab-webhooker
+
+This is a simple way to update your production code by GitLab triggers.
+Added token to web hook get URL. For example: `http://myapp.com:4400?token=qwerty123`. Look how to use GitLab web hooks on official manual here: https://gitlab.com/gitlab-org/gitlab-ce/blob/master/doc/web_hooks/web_hooks.md
+
+### How to start
+
+```javascript
+var webhooker = require('gitlab-webhooker');
+
+app.use(webhooker.init({
+  token: 'qwerty123', // Optional. Secret key
+  events: ['push', 'tag_push', 'issue', 'merge_request'], // Optional. Event list for track
+  command: 'cd ~/myapp; git pull origin master; if git diff --name-status HEAD HEAD~1 | grep -e package.json -e shrinkwrap.js; then npm install; fi' // Optional. Shell command when triggered
+}));
+```
+
+### NOTE:
+
+If you use forever don't forget to use .foreverignore file with `**/.git/**`. This will save you from false code restarting.
---
+++ index.js
@@ -0,0 +1,41 @@
+var app = require('orangebox').app(1);
+var exec = require('child_process').exec;
+var path = require('path');
+
+module.exports.init = function(opt) {
+
+  var mainAppPath = path.resolve(__dirname, '../../');
+
+  var config = {};
+  config.token = opt.token || '';
+  config.events = opt.events || ['push', 'merge_request'];
+  config.command = opt.command || 'cd ' + mainAppPath + '; git pull origin master; if git diff --name-status HEAD HEAD~1 | grep -e package.json -e shrinkwrap.js; then npm install; fi';
+
+  app.all('/', function(req, res) {
+
+    try {
+      var json = JSON.parse(Object.keys(req.body)[0]);
+    } catch (e) {
+      console.error('gitlab-hooker: Post data are not GitLab JSON');
+      var json = {};
+    }
+
+    if (config.token === req.query.token && config.events.indexOf(json.object_kind) !== -1) {
+
+      exec(config.command, function (err, stdout, stderr) {
+        if (stderr !== {} && err === null) {
+          res.status(200).send({ status: 'OK', stdout: stdout, stderr: stderr });
+        } else {
+          res.status(500).send({ status: 'Something wrong', stdout: stdout, stderr: stderr, err: err });
+        }
+      });
+
+    } else {
+      res.status(400).send({ status: 'Bad Request' });
+    }
+  });
+
+  app.listen(4400, function() {
+  	console.log('gitlab-webhooker started on http://localhost:4400');
+  });
+};
