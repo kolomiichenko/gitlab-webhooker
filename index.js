adb79d1db4c74e9393dbd97970df9b049187e2ff
Tobias Nendel <tobias.nendel@scubical.com>
2016-07-03 20:11:15 +0200

    adding some logging support

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var app = require('orangebox').app(1);
 var exec = require('child_process').exec;
 var path = require('path').resolve();
 var extend = require('util')._extend;
+var log = require('loglevel');
 
 module.exports.init = function(opt) {
 
@@ -13,16 +14,22 @@ module.exports.init = function(opt) {
     onEvent: null,
     command: 'cd ' + path + '; git pull origin master; if git diff --name-status HEAD HEAD~1 | grep -e package.json -e shrinkwrap.js; then npm update; fi',
     exit: false
+    logLevel: 'warn'
   };
 
   extend(config, opt);
 
+  log.setDefaultLevel(config.logLevel || 'warn')
   app.all('/', function(req, res) {
 
     try {
+      log.info('Request received!');
+      log.debug(req);
       var json = JSON.parse(Object.keys(req.body)[0]);
     } catch (e) {
       console.error('gitlab-webhooker: Post data are not GitLab JSON');
+      log.warn('gitlab-webhooker: Post data are not GitLab JSON');
+      log.debug(e);
       var json = {};
     }
 
@@ -48,10 +55,12 @@ module.exports.init = function(opt) {
 
     } else {
       res.status(400).send({ status: 'Bad request' });
+      log.warn("Bad request")
+      log.info(req)
     }
   });
 
   app.listen(config.port, function() {
-    console.log('gitlab-webhooker started on http://localhost:' + config.port);
+    log.info('gitlab-webhooker started on http://localhost:' + config.port);
   });
 };
